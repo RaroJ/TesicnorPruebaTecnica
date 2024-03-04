54a72b4c5a133a3d5aa12bbd24e424199b15b1f1
RaroJ <ramirezharoj@gmail.com>
2024-03-04 20:56:25 +0100

    Version 1.0 with complete functionality

--- src/main/java/com/harrypotterapp/tesicnorpruebatecnica/DB/DatabaseConnection.java
+++ src/main/java/com/harrypotterapp/tesicnorpruebatecnica/DB/DatabaseConnection.java
@@ -1,17 +1,33 @@
 package com.harrypotterapp.tesicnorpruebatecnica.DB;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
     private static Connection con = null;
 
     static
     {
-        String url = "jdbc:mysql:// localhost:3306/harrypottermdb";
-        String user = "root";
-        String pass = "tesicnordatabasetest";
+
+        Properties prop = new Properties();
+        String fileName = "src/main/java/com/harrypotterapp/tesicnorpruebatecnica/DB/db.config";
+        try (FileInputStream fis = new FileInputStream(fileName)) {
+            prop.load(fis);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = prop.getProperty("url");
+        String user = prop.getProperty("user");
+        String pass = prop.getProperty("pass");
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(url, user, pass);
