eba29811ce572305114985314f50f9adb6cedd77
RaroJ <ramirezharoj@gmail.com>
2024-03-04 21:43:30 +0100

    Version 1.0 with complete functionality

--- src/main/java/com/harrypotterapp/tesicnorpruebatecnica/DB/DatabaseConnection.java
+++ src/main/java/com/harrypotterapp/tesicnorpruebatecnica/DB/DatabaseConnection.java
@@ -29,7 +29,7 @@ public class DatabaseConnection {
         String pass = prop.getProperty("pass");
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(url, user, pass);
         }
         catch (ClassNotFoundException | SQLException e) {
